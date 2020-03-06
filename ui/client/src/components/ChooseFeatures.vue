2fee934f45e86d60107734cb3e2cf11f29e81807
Gosha <gyarduhin@gmail.com>
2020-03-06 16:51:55 +0300

    add structure

---
+++ ui/client/src/components/ChooseFeatures.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="choose">
+    <div class="playerChoose" >
+      <h2> Select player </h2>
+      <select>
+        <option v-for="player in players" v-bind:key="player.name">{{player.name}}</option>
+      </select>
+    </div>
+    <div class="teamChoose" >
+      <h2> Select team </h2>
+      <select>
+        <option v-for="team in teams" v-bind:key="team.name">{{team.name}}</option>
+      </select>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ChooseFeatures',
+  data() {
+    return {
+      players: [
+        { name: 'Ovechkin' },
+        { name: 'Malkin' },
+        { name: 'Nichushkin' },
+      ],
+      teams: [
+        { name: 'Detroit' },
+        { name: 'Monreal' },
+        { name: 'CSKA' },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
--- ui/client/src/views/Home.vue
+++ ui/client/src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1> Hello! </h1>
+    <ChooseFeatures />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import ChooseFeatures from '@/components/ChooseFeatures.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+    ChooseFeatures,
   },
 };
 </script>
